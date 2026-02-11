ed8d9be3517af2ede55302b6c8a4b284847ebc41
Rachel McMinn <rachelmcminn@Rachels-MacBook-Pro-2.local>
2026-02-11 14:21:19 -0700

    added relevant projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,26 @@
+
+    // {
+    //     id: '',
+    //     title: "",
+    //     description: "",
+    //     image: "",
+    //     completionDate: "",
+    //     githubRepoURL: "",
+    //     figmaURL: "",
+    //     role: "", // developer, designer, full-stack, front-end
+    //     techStack: [], // ["React", "Node.js", "PostgreSQL"]
+
+    // },
+// note - other attributes like liveDemoURL, impact, highlights, challenges, etc will be added on a project level basis for now. could extend this data structure in the future 
+
+
 export const projectsData = [
     {
         id: 'yippee-sudoku',
         title: "Yippee Sudoku!",
         description: "sudoku but cool",
         image: null,
-        completionDate: "null",
+        completionDate: "TBD",
         githubRepoURL: "https://github.com/rachelmcminn/sudoku-game",
 
     },
@@ -13,8 +29,62 @@ export const projectsData = [
         title: "JSM Intune Connect",
         description: "An integration tool rebuilt in React designed to seamlessly connect Microsoft Intune and Jira Service Management CMDB.",
         image: null,
-        completionDate: "null",
+        completionDate: "March 2025",
         githubRepoURL: "https://github.com/rachelmcminn/Intune-Connect",
+
+    },
+    {
+        id: 'sla-insights',
+        title: "SLA Insights for JSM",
+        description: "Lorem ipsum",
+        image: null,
+        completionDate: "February 2025",
+        githubRepoURL: "null",
+
+    },
+    {
+        id: 'k3-redesign',
+        title: "Kings III UI/UX Redesign",
+        description: "Lorem Ipsum",
+        image: null,
+        completionDate: "March 2025",
+        githubRepoURL: "null",
+
+    },
+    {
+        id: 'mbf-photo-stitching',
+        title: "MyBrandForce Photo Stitching",
+        description: "Lorem ipsum",
+        image: null,
+        completionDate: "October 2024",
+        githubRepoURL: "null",
+
+    },
+    {
+        id: 'rlid-redesign',
+        title: "Lane County RLID UI/UX Design and Development",
+        description: "Lorem ipsum",
+        image: null,
+        completionDate: "June 2024",
+        githubRepoURL: "null",
+
+    },
+    {
+        id: 'mbf-portal-dashboard',
+        title: "MyBrandForce Client Portal MVP Design and Development",
+        description: "Lorem ipsum",
+        image: null,
+        completionDate: "March 2024",
+        githubRepoURL: "null",
+
+    },
+    {
+        id: 'mbf-mobile-designs',
+        title: "MyBrandForce Mobile Application UI/UX Design",
+        description: "Lorem ipsum",
+        image: null,
+        completionDate: "November 2023",
+        githubRepoURL: "null",
 
     },
     {
